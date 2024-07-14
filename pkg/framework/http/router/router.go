915c9a2433a6067dd84b35634583a35799bde847
Mehrdad Mahdian <mahdian.mhd@gmail.com>
2024-07-14 17:29:46 +0200

    API-22: add middleware functionality

--- pkg/framework/http/router/router.go
+++ pkg/framework/http/router/router.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"net/http"
 	"strings"
+
+	"github.com/pyramid.io/planit-backend/pkg/framework/http/middleware"
 )
 var router *Router
 
@@ -11,20 +13,19 @@ func New() (RouterInterface, error) {
 	router = &Router{
 		routes: []RouteInterface{},
 	}
-
 	return router, nil
 }
 
-func GET(path string, handler http.HandlerFunc) Route {
-	return Route{
+func GET(path string, handler http.HandlerFunc) *Route {
+	return &Route{
 		path: path,
 		handler: handler,
 		httpMethod: http.MethodGet,
 	}
 }
 
-func POST(path string, handler http.HandlerFunc) Route {
-	return Route{
+func POST(path string, handler http.HandlerFunc) *Route {
+	return &Route{
 		path: path,
 		handler: handler,
 		httpMethod: http.MethodPost,
@@ -38,26 +39,37 @@ type RouterInterface interface {
 
 type RouteInterface interface {
 	GetPath() string
-	GetHandler() http.HandlerFunc
+	GetHandler() http.Handler
 	GetMethod() string
+	GetMiddlewares() []middleware.Middleware
 }
 
 type Route struct {
 	path string
-	handler http.HandlerFunc
+	handler http.Handler
 	httpMethod string
+	middlewares []middleware.Middleware
+}
+
+func (route *Route) Middlewares(middlewares ...middleware.Middleware) *Route {
+	route.middlewares = middlewares
+	return route
 }
 
 func (route Route) GetPath() string {
 	return route.path
 }
 
-func (route Route) GetHandler() http.HandlerFunc {
+func (route Route) GetHandler() http.Handler {
 	return route.handler
 }
 
 func (route Route) GetMethod() string {
 	return route.httpMethod
+}
+
+func (route Route) GetMiddlewares() []middleware.Middleware {
+	return route.middlewares
 }
 
 type Router struct {
@@ -105,10 +117,23 @@ func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 				ctx = context.WithValue(ctx, key, value)
 			}
 			req = req.WithContext(ctx)
-			handler := route.GetHandler()
-			handler(w, req)
+			handler := r.applyMiddlewares(route, )
+			handler.ServeHTTP(w, req)
 			return
 		}
 	}
 	http.NotFound(w, req)
+}
+
+func (r *Router) applyMiddlewares(route RouteInterface) http.Handler {
+
+	mergedMiddlewares := []middleware.Middleware{}
+	mergedMiddlewares = append(middleware.BasicMiddlewares, route.GetMiddlewares()...)
+	
+	handler := route.GetHandler()
+	for i := len(mergedMiddlewares) - 1; i >= 0; i-- { // Apply in reverse order
+		m := mergedMiddlewares[i]
+		handler = m(handler)
+	}
+	return handler
 }
---
+++ pkg/framework/session/session.go
@@ -0,0 +1,93 @@
+package session
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var serviceRegistry = map[string]DriverConstructor{
+	"filesystem": NewFileSystemSessionDriver,
+}
+
+type DriverKeyOrConstructor interface{}
+
+type DriverConstructor func(config map[string]interface{}) (SessionDriverInterface, error)
+
+type SessionServiceInterface interface {
+	Create(data map[string]interface{}, expiry *time.Time) (*Session, error)
+	Get(id string) (*Session, error)
+	Delete(id string) error
+}
+
+type SessionDriverInterface interface {
+	Create(key string, data map[string]interface{}, expiresAt *time.Time) (*Session, error)
+	Get(key string) (*Session, error)
+	Delete(key string) error
+}
+
+type Session struct {
+	ID string
+	Data map[string]interface{}
+	ExpiresAt *time.Time
+}
+
+type SessionService struct {
+	driver SessionDriverInterface
+}
+
+func New(driverKeyOrConstructor DriverKeyOrConstructor, driverConfig map[string]interface{}) (SessionServiceInterface, error) {
+	var constructor DriverConstructor
+	var ok bool
+
+	switch v := driverKeyOrConstructor.(type) {
+	case string:
+		constructor, ok = serviceRegistry[v]
+		if !ok {
+			return nil, errors.New("session driver could not be started")
+		}
+	case DriverConstructor:
+		constructor = v
+	default:
+		return nil, errors.New("invalid driver key or constructor")
+	}
+
+	driver, err := constructor(driverConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SessionService{
+		driver: driver,
+	}, nil
+}
+
+func (service *SessionService) Create(data map[string]interface{}, expiry *time.Time) (*Session, error) {
+	sessionID := uuid.New().String()
+
+	session, err := service.driver.Create(
+		sessionID,
+		data,
+		expiry,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+func (service *SessionService) Get(id string) (*Session, error) {
+	session, err := service.driver.Get(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+func (service *SessionService) Delete(id string) error {
+	return service.driver.Delete(id)
+}
